d23086e072172dfecae21d488bedf70d7630d072
Andi Radulescu <andi.radulescu@gmail.com>
2026-03-04 21:11:17 +0200

    update manifests from release branches (#191)

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
   manifests: {
-    release_mici: 'https://raw.githubusercontent.com/commaai/openpilot/ccf7361798a2b7ff36f5065dffb602eb40c22302/system/hardware/tici/all-partitions.json',
-    release_tizi: 'https://raw.githubusercontent.com/commaai/openpilot/927548621be1be0c2c9063868b93d1f5020904de/system/hardware/tici/all-partitions.json',
-    release_tici: 'https://raw.githubusercontent.com/commaai/openpilot/927548621be1be0c2c9063868b93d1f5020904de/system/hardware/tici/all-partitions.json',
+    release_mici: 'https://raw.githubusercontent.com/commaai/openpilot/release-mici/system/hardware/tici/all-partitions.json',
+    release_tizi: 'https://raw.githubusercontent.com/commaai/openpilot/release-tizi/system/hardware/tici/all-partitions.json',
+    release_tici: 'https://raw.githubusercontent.com/commaai/openpilot/release-tici/system/hardware/tici/all-partitions.json',
   },
   loader: {
     url: 'https://raw.githubusercontent.com/commaai/flash/master/src/QDL/programmer.bin',
